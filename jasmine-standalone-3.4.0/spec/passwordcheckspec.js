9c92a389c78cb460a3add767eeb2dde7a28930fe
rpanguana <mac@MacBook-Pro.local>
2019-08-16 12:03:28 +0200

    second commit

--- jasmine-standalone-3.4.0/spec/passwordcheckspec.js
+++ jasmine-standalone-3.4.0/spec/passwordcheckspec.js
@@ -1,7 +1,12 @@
 describe("password_is_valid", function() {
   
-  it("should return true if all conditions are met", function() {
+  it("If a condition is fails then an error/exception should be raised/thrown", function() {
     expect(password_is_valid("Rulani123")).toBe("Great!! password is strong enough");
   });
 
+describe("password_is_ok", function() {
+  
+  it("if the given password meets at least three of the criteria listed above then this function should return true", function() {
+    expect(password_is_ok("Rulani123")).toBe("password is okay");
+  })});
 });
--- jasmine-standalone-3.4.0/src/passwordcheck.js
+++ jasmine-standalone-3.4.0/src/passwordcheck.js
@@ -1,29 +1,38 @@
 	var password = prompt("");
+	var num = /[0-9]/;
+	var uppercase = /[A-Z]/;
+	var lowercase = /[a-z]/;
+
+	var number = num.test(password);
+	var up = uppercase.test(password);
+	var low = lowercase.test(password);
+
+		
 	function password_is_valid(password){
 		
-		var num = /[0-9]/;
-		var uppercase = /[A-Z]/;
-		var lowercase = /[a-z]/;
 
-		var number = num.test(password);
-		var up = uppercase.test(password);
-		var low = lowercase.test(password);
 
 
 		if ( password != "" && password.length >= 8 && number && up && low){
 			return("Great!! password is strong enough");
 		}
-		// else if ( password != [a-z] || password != [A-Z]){
-		// 	alert("password should contain atleast 1 lowercase & 1 uppercase letter");
-		// }
 
-		// else if ( password.length <= 8 ){
-		// 	return("password should contain atleast 8 characters");}
-
-		// else if (password != a ){
-		// 	return("password should contain a number,atleast 1 lowercase & 1 uppercase letter ");}
 
 		else {
-			return("review your password");}
+			return("please review your password");}
 		}
 		password_is_valid(password);
+
+	function password_is_ok(password){
+
+		if ( password != "" && password.length >= 8 && (number || up || low )){
+			return("password is okay");
+		}
+		else {
+			return("please review your password");
+		}
+	}
+
+// 	Next, implement a function called password_is_ok(password). If the given password meets at least three of the criteria listed above then this function should return true, otherwise it should return false.
+
+// Add a feature: the password is never OK if conditions 1 and 2 are not met
