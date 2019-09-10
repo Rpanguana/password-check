3c2603f7c54303c6ddd13fe9482bb9c1f7975a5b
rpanguana <mac@MacBook-Pro.local>
2019-09-10 16:20:31 +0200

    modifications made

--- jasmine-standalone-3.4.0/spec/passwordcheckspec.js
+++ jasmine-standalone-3.4.0/spec/passwordcheckspec.js
@@ -1,12 +1,19 @@
 describe("password_is_valid", function() {
   
-  it("If a condition is fails then an error/exception should be raised/thrown", function() {
-    expect(password_is_valid("Rulani123")).toBe("Great!! password is strong enough");
+  it("If a condition fails then an error/exception should be raised/thrown", function() {
+    expect(password_is_valid("Rulani123")).tobe("Great!! password is strong enough");
   });
+
+  it("password should not be null", function (){
+       expect(function () {
+           let output = password_is_valid(null);
+       }).toThrow(new Error("Password must contain atleast one uppercase letter, lowercase letter and a number"));
+   });
 
 describe("password_is_ok", function() {
   
   it("if the given password meets at least three of the criteria listed above then this function should return true", function() {
     expect(password_is_ok("Rulani123")).toBe("password is okay");
   })});
+
 });
--- jasmine-standalone-3.4.0/src/passwordcheck.js
+++ jasmine-standalone-3.4.0/src/passwordcheck.js
@@ -1,4 +1,4 @@
-	var password = prompt("");
+	var password = ("Rulani1234");
 	var num = /[0-9]/;
 	var uppercase = /[A-Z]/;
 	var lowercase = /[a-z]/;
@@ -6,33 +6,29 @@
 	var number = num.test(password);
 	var up = uppercase.test(password);
 	var low = lowercase.test(password);
+	
 
 		
 	function password_is_valid(password){
-		
-
-
 
 		if ( password != "" && password.length >= 8 && number && up && low){
 			return("Great!! password is strong enough");
 		}
-
-
 		else {
-			return("please review your password");}
+			throw new Error ("please review your password");
 		}
-		password_is_valid(password);
+		}
+
+	console.log(password_is_valid(password));
+
 
 	function password_is_ok(password){
 
 		if ( password != "" && password.length >= 8 && (number || up || low )){
-			return("password is okay");
+			return true;
 		}
 		else {
-			return("please review your password");
+			return false;
 		}
 	}
-
-// 	Next, implement a function called password_is_ok(password). If the given password meets at least three of the criteria listed above then this function should return true, otherwise it should return false.
-
-// Add a feature: the password is never OK if conditions 1 and 2 are not met
+	console.log(password_is_ok(password));
