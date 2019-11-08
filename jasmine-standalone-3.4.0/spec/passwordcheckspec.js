b53c9d7b8435e736641a5723f28470c146d31786
Rpanguana <filipe.panguana@outlook.com>
2019-11-08 14:21:27 +0200

    made some modifications on the source and the spec

--- jasmine-standalone-3.4.0/spec/passwordcheckspec.js
+++ jasmine-standalone-3.4.0/spec/passwordcheckspec.js
@@ -1,19 +1,19 @@
 describe("password_is_valid", function() {
   
-  it("If a condition fails then an error/exception should be raised/thrown", function() {
-    expect(password_is_valid("Rulani123")).tobe("Great!! password is strong enough");
+  it("if password contains all required characters then it should return successful", function() {
+    expect(password_is_valid("Rulani123!")).toBe(true);
   });
-
+ 
   it("password should not be null", function (){
        expect(function () {
            let output = password_is_valid(null);
-       }).toThrow(new Error("Password must contain atleast one uppercase letter, lowercase letter and a number"));
+       }).toThrow(new Error("Password must contain atleast (1) uppercase letter, (1) lowercase letter and a number. Please review your password"));
    });
 
 describe("password_is_ok", function() {
   
   it("if the given password meets at least three of the criteria listed above then this function should return true", function() {
-    expect(password_is_ok("Rulani123")).toBe("password is okay");
+    expect(password_is_ok("Rulani123")).toBe(true);
   })});
 
 });
--- jasmine-standalone-3.4.0/src/passwordcheck.js
+++ jasmine-standalone-3.4.0/src/passwordcheck.js
@@ -1,25 +1,27 @@
-	var password = ("Rulani1234");
+	var password = ("Rulani1234!");
 	var num = /[0-9]/;
 	var uppercase = /[A-Z]/;
 	var lowercase = /[a-z]/;
+	var specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
 	var number = num.test(password);
 	var up = uppercase.test(password);
 	var low = lowercase.test(password);
+	var spclChar = specialChar.test(password);
 	
 
 		
-	function password_is_valid(password){
+function password_is_valid(password){
 
-		if ( password != "" && password.length >= 8 && number && up && low){
-			return("Great!! password is strong enough");
-		}
-		else {
-			throw new Error ("please review your password");
-		}
-		}
+	if ( password != null && password.length >= 8  && number && up && low && spclChar){
+		return true;
+	}
+	else {
+		throw new Error ("Password must contain atleast (1) uppercase letter, (1) lowercase letter and a number. Please review your password");
+	}
+	}
 
-	console.log(password_is_valid(password));
+console.log(password_is_valid(password));
 
 
 	function password_is_ok(password){
